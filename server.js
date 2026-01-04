70e127fc3efde1b3e61ae69788b9c789dbdebd7d
comeobama4-sketch <comeobama4@gmail.com>
2026-01-04 20:24:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const CALLBACK_URL = process.env.CALLBACK_URL || "https://abels-test-stk-push.on
 const receiptsFile = path.join(__dirname, "receipts.json");
 
 // CORS origin: keep frontend intact
-const FRONTEND_ORIGIN = "https://finance-tech-vowr.onrender.com";
+const FRONTEND_ORIGIN = "https://fuliza-inc.vercel.app/";
 
 // Middleware
 app.use(bodyParser.json());
